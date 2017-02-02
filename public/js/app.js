4c3033987214fd462728ccdd142248def9e6fcff
Architvaidya <archit.vaidya@gmail.com>
2017-02-01 19:43:37 -0500

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,7 @@ var socket = io();
 socket.on('connect',function(){
 	console.log('Client connected to the server');
 });
+
+socket.on('message', function(message){
+	console.log('New message is: '+message.text);
+});
